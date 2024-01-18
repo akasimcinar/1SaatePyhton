85a147a1b33c2b58f50b11016ab55b6b737912bb
akasimcinar <kasimparmaksiz@gmail.com>
2024-01-18 11:26:01 +0300

    Initial commit

---
+++ 1 satte pyhton.py
@@ -0,0 +1,66 @@
+# print("Hello World")
+#
+# x = "Kasım" #string
+# c = 123.5
+# AldıMI= True
+#
+# isim = "Moby Dick"
+# sayfa = 195
+# agirlik = 13.45
+# YeniMi = True
+#
+# print(isim)
+# print("isim")
+# print(type(agirlik))
+#
+# isim = input("İsminiz Nedir? ")
+#
+# print("Merhaba " + isim)
+# print("Selam " + isim)
+# print("Naber " + isim + " ? ")
+#
+# isim = input(" İsminiz Nedir ? ")
+#
+# yemek = input(" Sevdiğiniz yemek nedir ? ")
+#
+# print (isim  + " " +yemek + " sever. ")
+#
+# isim = "Kasım"
+#
+# yas = 28
+#
+# print (isim + " " + str(yas) + " " + "yaşındadır." )
+#
+# int()
+# float()
+# bool()
+# str()
+#
+# x = 9
+# y = 5
+#
+# print(x + y)
+# print(x - y)
+# print(x * y)
+# print(x / y)
+# print(x // y)
+# print(x % y)
+# print(x ** y)
+#
+# x = input("birinci Sayı giriniz ")
+# y = input("İkinci Sayı giriniz ")
+#
+# print(int(x)+int(y))
+#
+#
+# print(round(9.8))
+# # print(abs(-9))
+#
+# import math
+# print(math.sqrt(9))
+#
+# print(min(9,10,56))
+# print(max(21,44,67))
+
+
+
